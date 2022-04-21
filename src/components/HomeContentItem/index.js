f8bc756ef80589bf89bcdb2e5ccc03a35bd22a1b
Carlos && Ponce <srtager555@gmail.com>
2022-04-21 02:43:11 -0600

    I created the HomeContentItem structure for the css

--- src/components/HomeContentItem/index.js
+++ src/components/HomeContentItem/index.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from "react";
 
-export function HomeContentItem() {
-  return (
-    <div>HomeContentItem</div>
-  )
+export function HomeContentItem({ title, description }) {
+   return (
+      <div className="HomeContentItem__container">
+         <div className="HomeContentItem__container--title">
+            <h2>{title()}</h2>
+         </div>
+         <div className="HomeContentItem__container--description">
+            <p>{description}</p>
+         </div>
+      </div>
+   );
 }
