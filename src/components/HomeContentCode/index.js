5ba3ed317a5e1d25bdabec43847336148dd08c54
Carlos && Ponce <srtager555@gmail.com>
2022-04-21 02:59:54 -0600

    the structure of HomeCode is ready

--- src/components/HomeContentCode/index.js
+++ src/components/HomeContentCode/index.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useRef } from "react";
 
-export function HomeContentCode() {
-  return (
-    <div>HomeContentCode</div>
-  )
+import { useCopy } from "@Hooks/useCopy";
+
+export function HomeContentCode({ code }) {
+   const codeRef = useRef(null);
+
+   return (
+      <div className="HomeContentCode__container">
+         <button
+            className="HomeContentCode__container--buttonCopy"
+            onClick={() => useCopy(codeRef)}
+         >
+            Copy
+         </button>
+         <div className="HomeContentCode__container--code">
+            <pre>
+               <code ref={codeRef}>{code}</code>
+            </pre>
+         </div>
+      </div>
+   );
 }
