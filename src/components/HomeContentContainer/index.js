168e858ecac2bd7098761144458cfa511d65d58f
Carlos && Ponce <srtager555@gmail.com>
2022-04-21 02:44:38 -0600

    I initializated and imported the css of the HomeItem

--- src/components/HomeContentContainer/index.js
+++ src/components/HomeContentContainer/index.js
@@ -3,6 +3,8 @@ import React, { useState, useEffect } from 'react'
 import { HomeContentCode } from '@Components/HomeContentCode'
 import { HomeContentItem } from '@Components/HomeContentItem'
 
+import "@Styles/HomeContent.css"
+
 export function HomeContentContainer({ component, code, color}) {
     // aqui tiene que hacerse una llamada a la api de object intersection con el div HomeContentContainer
     // cuando sea observado se cambiara el color del fondo del body con un className
