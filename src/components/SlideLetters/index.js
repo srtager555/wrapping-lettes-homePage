4f69131486d8cd328f9b5e4d890e128702ba99f0
Carlos && Ponce <srtager555@gmail.com>
2022-04-22 03:34:22 -0600

    a

--- src/components/SlideLetters/index.js
+++ src/components/SlideLetters/index.js
@@ -36,7 +36,7 @@ export function SlideLetters({ sLettersOpts = {}, scrollPosition }) {
       setSlide6([Slide6[0], Slide6[1]]);
    }, [sLettersOpts]);
 
-   useEffect(() => {
+   useLayoutEffect(() => {
       arrSlide.forEach((element, index) => {
          if (scrollPosition >= document.body.clientHeight - window.innerHeight * 1.05) {
             if (element[0][0] === true) element[1]([true, true]);
