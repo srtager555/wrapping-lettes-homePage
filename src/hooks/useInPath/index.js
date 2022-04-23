a70e6889b9c29e444eb6aa6f83b8b6bbda61e20d
Carlos && Ponce <srtager555@gmail.com>
2022-04-23 02:40:47 -0600

    log removed

--- src/hooks/useInPath/index.js
+++ src/hooks/useInPath/index.js
@@ -2,7 +2,6 @@ import { useEffect } from "react"
 
 export function useInPath(state) {
     useEffect(() => {
-        console.log(window.location.pathname)
         state(window.location.pathname)
     }, [])
 }
