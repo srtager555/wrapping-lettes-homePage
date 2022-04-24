101bba6d7bc95d0d3b053842feb8c5cdb42d5355
Carlos && Ponce <srtager555@gmail.com>
2022-04-23 22:52:33 -0600

    I soved a problem with the nav

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -7,19 +7,51 @@ import "@Styles/Navbar.css";
 export function Navbar({ path, scrollPosition }) {
    const [navState, setNavState] = useState("default");
    const [isVisible, setIsVisible] = useState(false);
+   const [openMobileMenu, setOpenMobileMenu] = useState(false);
 
    function navContent(navState) {
       return navState;
    }
 
+   function handleActiveMobileMenu() {
+      setOpenMobileMenu(!openMobileMenu);
+   }
+
+   const a = (
+      <>
+         <a
+            href="https://github.com/srtager555/wrapping-letters"
+            target="_blanks"
+         >
+            GitHub
+         </a>
+         <Link to="/docs">NPM</Link>
+      </>
+   );
+
+   const b = (
+      <>
+         <a
+            href="https://github.com/srtager555/wrapping-letters"
+            target="_blanks"
+         >
+            GitHub
+         </a>
+         <Link to="/docs">Documentation</Link>
+      </>
+   );
+
    useEffect(() => {
-      if (scrollPosition >= document.body.clientHeight - window.innerHeight)
-         setIsVisible(false);
-      else if (scrollPosition > window.innerHeight) setIsVisible(true);
-      else setIsVisible(false);
+      if (path === "/") {
+         if (scrollPosition >= document.body.clientHeight - window.innerHeight)
+            setIsVisible(false);
+         else if (scrollPosition > window.innerHeight) setIsVisible(true);
+         else setIsVisible(false);
+      }
    }, [scrollPosition]);
 
    useEffect(() => {
+      console.log(path);
       switch (path) {
          case "/":
             setNavState(b);
@@ -31,13 +63,18 @@ export function Navbar({ path, scrollPosition }) {
 
          default:
             setNavState(a);
+            setIsVisible(true);
             break;
       }
    }, [path]);
 
    return (
       <>
-         <nav className={`navbar-container${isVisible ? " show" : ""}`}>
+         <nav
+            className={`navbar-container${isVisible ? " show" : ""}${
+               path != "/" ? " docs" : ""
+            } `}
+         >
             {path === "/" ? (
                <span className="wrapping-letters--logo">WL</span>
             ) : (
@@ -48,6 +85,15 @@ export function Navbar({ path, scrollPosition }) {
             <div className="navbar-container__content">
                {/* <Link to="">Getting started</Link> */}
                {navContent(navState)}
+               <div
+                  className={`navbar-menu__button${
+                     openMobileMenu ? " active" : ""
+                  }`}
+                  onClick={handleActiveMobileMenu}
+               >
+                  <span className="navbar-menu__button--line"></span>
+                  <span className="navbar-menu__button--line"></span>
+               </div>
             </div>
          </nav>
          {a === setNavState ? (
@@ -56,24 +102,3 @@ export function Navbar({ path, scrollPosition }) {
       </>
    );
 }
-
-const a = (
-   <>
-      <a href="https://github.com/srtager555/wrapping-letters" target="_blanks">
-         GitHub
-      </a>
-      <Link to="/docs">Npm</Link>
-      <div className="navbar-menu__button">
-         <span className="navbar-menu__button--line"></span>
-         <span className="navbar-menu__button--line"></span>
-      </div>
-   </>
-);
-const b = (
-   <>
-      <a href="https://github.com/srtager555/wrapping-letters" target="_blanks">
-         GitHub
-      </a>
-      <Link to="/docs">Documentation</Link>
-   </>
-);
