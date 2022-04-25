0c13725752995cd3dbd2587ae3f9f755192b0d23
Carlos && Ponce <srtager555@gmail.com>
2022-04-25 16:37:33 -0600

    page about 404 ready

--- src/pages/notFound/index.js
+++ src/pages/notFound/index.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
 
-export default function NotFound() {
-  return (
-    <div>NotFound</div>
-  )
+import "@Styles/NotFound.css"
+
+export default function NotFound({ setSLettersOpts }) {
+   useEffect(() => {
+      setSLettersOpts({ 
+          Slide1: [true, false], 
+          Slide2: [true, false],
+          Slide3: [true, false],
+          Slide4: [true, false],
+          Slide5: [true, false],
+          Slide6: [true, false]
+        });
+   }, []);
+   return (
+      <div className="notFound_container">
+         <div className="notFound__container-404">
+            <span className="notFound__container-404__404">404</span>
+            <span className="notFound__container-404__notMatch">not match</span>
+         </div>
+         <div className="notFound__container-links">
+            <Link to="/">Return to Home </Link>
+         </div>
+      </div>
+   );
 }
