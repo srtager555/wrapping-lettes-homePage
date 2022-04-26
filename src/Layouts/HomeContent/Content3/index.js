79296910efd1f9503ae39faaa50d3f7326c9f006
Carlos && Ponce <srtager555@gmail.com>
2022-04-26 00:01:11 -0600

    code for the 3# section of home, ready

--- src/Layouts/HomeContent/Content3/index.js
+++ src/Layouts/HomeContent/Content3/index.js
@@ -1,1 +1,77 @@
+import React from "react";
+import WL from "wrapping-letters-react";
 
+function Container() {
+   function ReactComponent({ letter, cssClass, ...props }) {
+      return (
+         <div className={`container-letter ${cssClass}`} {...props}>
+            <span className={`letter-wrapp ${cssClass}`}>{letter}</span>
+         </div>
+      );
+   }
+
+   return (
+      <div className="letter-container">
+         <WL
+            word="Create the packaging of your choice"
+            wordOptions={[
+               {
+                  ClassToAdd: "letter-wrapp",
+
+                  SelectClass: {
+                     wordToSearch: "packaging ",
+                     classToAdd: "pack-letter",
+                  },
+                  PerWord: true,
+               },
+            ]}
+            structure={ReactComponent}
+         />
+      </div>
+   );
+}
+
+export function Content3() {
+   return [
+      [
+         <Container />,
+         "The <span /> is not functional for your component? No problem, you can create a special structure for your component",
+      ],
+      `
+import WL from "wrapping-letters-react";
+
+function ReactComponent({ letter, cssClass, ...props }) {
+    return (
+        <div 
+            className={\`container-letter \${cssClass}\`} 
+            {...props}
+        >
+            <span className={\`letter-wrapp \${cssClass}\`}>
+                {letter}
+            </span>
+        </div>
+    );
+}
+
+return (
+    <div className="letter-container">
+        <WL
+            word="Create the packaging of your choice"
+            wordOptions={[
+                {
+                    ClassToAdd: "letter-wrapp",
+
+                    SelectClass: {
+                        wordToSearch: "packaging ",
+                        classToAdd: "pack-letter",
+                    },
+                    PerWord: true,
+                },
+            ]}
+            structure={ReactComponent}
+        />
+</div>
+);
+      `,
+   ];
+}
