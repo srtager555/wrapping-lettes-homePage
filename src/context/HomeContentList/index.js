7e76ae962104ab9cd344bd8b80d5e9912bfac560
Carlos && Ponce <srtager555@gmail.com>
2022-04-26 00:01:52 -0600

    Array list with the content of home upgraded

--- src/context/HomeContentList/index.js
+++ src/context/HomeContentList/index.js
@@ -1,8 +1,11 @@
 import { Content1 } from "@HomeLayout/Content1";
+import { Content2 } from "@HomeLayout/Content2";
+import { Content3 } from "@HomeLayout/Content3";
+import { Content4 } from "@HomeLayout/Content4";
 
 export const arrContent = [
     Content1(),
-    Content1(),
-    Content1(),
-    Content1(),
+    Content2(),
+    Content3(),
+    Content4(),
 ]
